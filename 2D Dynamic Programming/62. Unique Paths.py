ce2b233430e54e1031a21bb9e99cd011275bb7db
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-03-14 13:26:16 +0000

    unique paths amended

--- 2D Dynamic Programming/62. Unique Paths.py
+++ 2D Dynamic Programming/62. Unique Paths.py
@@ -2,7 +2,13 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         if m == n == 1:
             return 1
-        dp = [[1] * m] * n
+        dp = []
+        for i in range(n):
+            row = []
+            for j in range(m):
+                row.append(1)
+            dp.append(row)
+
         for row in range(1, len(dp)):
             for col in range(1, len(dp[row])):
                 dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
