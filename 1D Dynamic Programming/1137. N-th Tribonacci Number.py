30f765cc4348fcf0a63c55e017e8906041a6450b
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-03-14 12:51:16 +0000

    nth tribonacci number

---
+++ 1D Dynamic Programming/1137. N-th Tribonacci Number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        sequence = [0, 1, 1]
+        for i in range(3, n + 1):
+            sequence.append(sequence[i - 3] + sequence[i - 2] + sequence[i - 1])
+        return sequence[n]
+
+# -------------------Better Space Optimization------------------------
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n < 3:
+            return 1
+        t0, t1, t2 = 0, 1, 1
+        for i in range(3, n + 1):
+            tn = t0 + t1 + t2
+            t0, t1, t2 = t1, t2, tn
+        return tn
