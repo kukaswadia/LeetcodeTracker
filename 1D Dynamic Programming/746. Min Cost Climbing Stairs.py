c99a2368919066f6b6346d33336ed4e99fb45900
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-03-14 09:23:20 +0000

    min cost climbing stairs

---
+++ 1D Dynamic Programming/746. Min Cost Climbing Stairs.py
@@ -0,0 +1,5 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for step in range(2, len(cost)):
+            cost[step] += min(cost[step - 1], cost[step - 2])
+        return min(cost[-1], cost[-2])
