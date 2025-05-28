8f92b8beed0768cef97c4c953dad44f807a84355
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-05-28 06:29:33 +0100

    Reorder routes to make all paths

--- Graphs/1466. Reorder Routes to Make All Paths Lead to the City Zero.py
+++ Graphs/1466. Reorder Routes to Make All Paths Lead to the City Zero.py
@@ -1,47 +1,25 @@
-# ----------------------------- DFS -----------------------------
-
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph = {i : [] for i in range(n)}
-        for u, v in connections:
-            graph[u].append((v, 1))
-            graph[v].append((u, 0))
+        neighbors = defaultdict(list)
+        connection = set()
+        for start, end in connections:
+            neighbors[start].append(end)
+            neighbors[end].append(start)
+            connection.add((start, end))
 
-        result = 0
+        curr = [0]
+        reverse = 0
         visited = set()
+        visited.add(0)
 
-        def dfs(city):
-            nonlocal result
-            visited.add(city)
-            for neighbour, direction in graph[city]:
-                if neighbour not in visited:
-                    if direction == 1:
-                        result += 1
-                    dfs(neighbour)
-
-        dfs(0)
-        return result
-
-# ----------------------------- BFS -----------------------------
-
-class Solution:
-    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph = {i : [] for i in range(n)}
-        for u, v in connections:
-            graph[u].append((v, 1))
-            graph[v].append((u, 0))
-
-        result = 0
-        visited = set([0])
-        queue = deque([0])
-
-        while queue:
-            city = queue.popleft()
-            for neighbour, direction in graph[city]:
-                if neighbour not in visited:
-                    if direction == 1:
-                        result += 1
-                    visited.add(neighbour)
-                    queue.append(neighbour)
-
-        return result
+        while curr:
+            new_curr = []
+            for city in curr:
+                for n in neighbors[city]:
+                    if n not in visited:
+                        visited.add(n)
+                        if (n, city) not in connection:
+                            reverse += 1
+                        new_curr.append(n)
+            curr = new_curr
+        return reverse
