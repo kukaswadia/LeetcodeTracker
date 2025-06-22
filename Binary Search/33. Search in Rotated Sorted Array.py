c8b0bcf02ef5fc443f9927d84662e94f6c3ec946
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-06-22 15:39:23 +0100

    Search in rotated sorted array

--- Binary Search/33. Search in Rotated Sorted Array.py
+++ Binary Search/33. Search in Rotated Sorted Array.py
@@ -3,19 +3,17 @@ class Solution:
         beg, end = 0, len(nums) - 1
         while beg < end:
             mid = (beg + end) // 2
-
             if nums[mid] == target:
                 return mid
-
-            if nums[beg] <= nums[mid]:
+            if nums[mid] >= nums[beg]:
                 if nums[beg] <= target <= nums[mid]:
                     end = mid - 1
                 else:
                     beg = mid + 1
-            else:
+            elif nums[mid] <= nums[end]:
                 if nums[mid] <= target <= nums[end]:
                     beg = mid + 1
                 else:
                     end = mid - 1
 
-        return -1 if nums[end] != target else end
+        return -1 if nums[end] != target else beg
