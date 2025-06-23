2183be2162dc98f96fb9ea862279e351965f4589
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-06-23 18:25:33 +0100

    Seaarch Insert Position

--- Binary Search/35. Search Insert Position.py
+++ Binary Search/35. Search Insert Position.py
@@ -1,34 +1,12 @@
-# ---------------------------------------------------------------------
-# Iterative Solution
-
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
+        low, high = 0, len(nums) 
+        while low < high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
                 return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return left
-
-# ---------------------------------------------------------------------
-# Recursive Solution
-
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        def find(left, right):
-            if left > right:
-                return left
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-
-            elif nums[mid] < target:
-                return find(mid + 1, right)
-            else:
-                return find(left, mid - 1)
-
-        return find(0, len(nums) - 1)
+            if target < nums[mid]:
+                high = mid
+            elif target > nums[mid]:
+                low = mid + 1
+        return low
