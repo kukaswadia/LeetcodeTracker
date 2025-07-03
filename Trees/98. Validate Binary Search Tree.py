1ddedd5514f17b22b152c16bf07afac0ffabd503
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-07-03 15:30:02 +0100

    Validate BST

--- Trees/98. Validate Binary Search Tree.py
+++ Trees/98. Validate Binary Search Tree.py
@@ -1,53 +1,18 @@
-# -------------------------------- Recursive Range Check --------------------------------
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        def validate(node, low, high):
-            if not node:
+        def helper(root, left, right):
+            if root is None:
                 return True
-            if not (low < node.val < high):
+            if not (left < root.val < right):
                 return False
 
-            return validate(node.left, low, node.val) and validate(node.right, node.val, high)
-        return validate(root, float('-inf'), float('inf'))
+            return helper(root.left, left, root.val) and helper(root.right, root.val, right)
 
-# -------------------------------- Recursive Inorder Traversal --------------------------------
-
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-
-        def validate(node):
-            if not node:
-                return []
-            return validate(node.left) + [node.val] + validate(node.right)
-
-        inorder_list = validate(root)
-
-        for i in range(1, len(inorder_list)):
-            if inorder_list[i] <= inorder_list[i - 1]:
-                return False
-        return True
-
-# -------------------------------- Iterative Inorder Traversal --------------------------------
-
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        stack = []
-        prev_val = None
-
-        while stack or root:
-            while root:
-                stack.append(root)
-                root = root.left
-
-            node = stack.pop()
-            if prev_val is not None and node.val <= prev_val:
-                return False
-
-            prev_val = node.val
-            root = node.right
-
-        return True
+        return helper(root, float('-inf'), float('inf'))
