34b72efc993db49c160cc679c437b0c24fe1bce3
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-07-13 15:31:04 +0100

    Valid Parentheses

--- Stack/20. Valid Parentheses.py
+++ Stack/20. Valid Parentheses.py
@@ -3,9 +3,9 @@ class Solution:
         d = {"(": ")", "{": "}", "[": "]"}
         stack = []
         for char in s:
-            if char in d:
+            if char in d:  # every single open parentheses is a key in the dictionary
                 stack.append(char)
             else:
-                if stack == [] or d[stack.pop()] != char:
+                if stack == [] or d[stack.pop()] != char: # check the value of that key in the dictionary
                     return False
         return True if stack == [] else False
