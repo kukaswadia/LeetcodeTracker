0c9dc51ac8c751642ca59215b342c6cbe370f44c
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-11-22 08:47:29 +0000

    Rotate Image

--- Arrays and Hashing/48. Rotate Image.py
+++ Arrays and Hashing/48. Rotate Image.py
@@ -3,10 +3,12 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
 
+        n = len(matrix)
         for i in range(n):
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
         for i in range(n):
-            matrix[i].reverse()
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
