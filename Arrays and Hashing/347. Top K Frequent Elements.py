aee29b3b1e793328402332f98e630cc5de759f92
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-12-01 14:17:25 +0000

    Top K frequent elements

--- Arrays and Hashing/347. Top K Frequent Elements.py
+++ Arrays and Hashing/347. Top K Frequent Elements.py
@@ -1,16 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
-        n = len(nums)
+        count = Counter(nums)
 
-        buckets = [[] for _ in range(n + 1)]
+        # Create buckets where index = frequency
+        buckets = [[] for _ in range(len(nums) + 1)]
 
-        for num, count in freq.items():
-            buckets[count].append(num)
-        res = []
-        for f in range(n, 0, -1):
-            for num in buckets[f]:
-                res.append(num)
-                if len(res) == k:
-                    return res
-        return
+        # Place each number in its frequency bucket
+        # items() method of a Python dictionary returns a list of tuples for each key-value pair in a dictionary
+        for num, freq in count.items():
+            buckets[freq].append(num)
+
+        # Collect k most frequent elements from highest frequency buckets
+        result = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+
+        return result
