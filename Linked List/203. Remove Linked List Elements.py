9697a415622ae05a908453b1ba65652e87c4ee22
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-12-03 18:05:13 +0000

    Remove linked list elements

--- Linked List/203. Remove Linked List Elements.py
+++ Linked List/203. Remove Linked List Elements.py
@@ -5,15 +5,32 @@
 #         self.next = next
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        curr = head
-        dummy = ListNode(None)
-        dummy.next = head
-        prev = dummy
-        while curr:
-            if curr.val == val:
-                prev.next = curr.next
-            else:
-                prev = curr
+        """
+        Iterative Solution
+        Time Complexity: O(n) - visit each node once
+        Space Complexity: O(1) - only use a few pointers
+        """
 
-            curr = curr.next
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy 
+
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
         return dummy.next
+
+        """
+        Recursive Solution
+        Time Complexity: O(n)
+        Space Complexity: O(n) - due to recursive stack
+        """
+
+        if not head:
+            return None
+
+        head.next = self.removeElements(head.next, val)
+        return head.next if head.val == val else head
