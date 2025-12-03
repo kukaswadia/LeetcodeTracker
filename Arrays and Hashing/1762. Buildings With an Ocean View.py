7d3abf539d3050f9e6bcf599e89a519b8ef82dba
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-12-03 19:16:52 +0000

    Buildings with an ocean view

--- Arrays and Hashing/1762. Buildings With an Ocean View.py
+++ Arrays and Hashing/1762. Buildings With an Ocean View.py
@@ -1,21 +1,15 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        buildings = []
-        for i, height in enumerate(heights):
-            while buildings and heights[buildings[-1]] <= height:
-                buildings.pop()
-            buildings.append(i)
-        return buildings
-
-------------------------------------------------------------------------------------
-
-class Solution:
-    def findBuildings(self, heights: List[int]) -> List[int]:
-        buildings = [len(heights) - 1]
-        max_height = heights[-1]
-        for i in range(len(heights) -1, -1, -1):
-            height = heights[i]
-            if height > max_height:
-                buildings.append(i)
-                max_height = height
-        return buildings[::-1]
+        result = []
+        max_height = 0
+        
+        # Traverse from right to left
+        for i in range(len(heights) - 1, -1, -1):
+            # If current building is taller than all buildings to its right
+            if heights[i] > max_height:
+                result.append(i)
+                max_height = heights[i]
+        
+        # Reverse to get indices in ascending order
+        result.reverse()
+        return result
--- Arrays and Hashing/383. Ransom Note.py
+++ Arrays and Hashing/383. Ransom Note.py
@@ -1,5 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        """
+        Time: O(m + n) where m = len(ransomNote), n = len(magazine)
+        Space: O(1) - at most 26 lowercase letters
+        """
         magazine_count = Counter(magazine)
         for char in ransomNote:
             if magazine_count[char] <= 0:
