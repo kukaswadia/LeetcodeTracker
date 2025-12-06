fcb79c1c0ed0dd39810e0d77f1215ec38f15c053
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-12-06 22:20:21 +0000

    Happy Number

--- Arrays and Hashing/202. Happy Number.py
+++ Arrays and Hashing/202. Happy Number.py
@@ -1,18 +1,25 @@
 class Solution:
+    def isHappy(self, n: int) -> bool:
+        """
+        Time Complexity: O(log n)
+        Space Complexity: O(1) - constant space
+        """
+        slow = n 
+        fast = n
 
-    def calc(self, n):
-        curr = 0
+        while True:
+            slow = self.get_sum_of_squares(slow)
+            fast = self.get_sum_of_squares(self.get_sum_of_squares(fast))
+
+            if fast == 1:
+                return True
+            if slow == fast:
+                return False
+
+    def get_sum_of_squares(self, n):
+        total = 0
         while n > 0:
             digit = n % 10
-            curr += digit * digit
+            total += digit * digit 
             n //= 10
-        return curr
-
-    def isHappy(self, n: int) -> bool:
-        seen = set()
-        while n != 1:
-            n = self.calc(n)
-            if n in seen:
-                return False
-            seen.add(n)
-        return True
+        return total
