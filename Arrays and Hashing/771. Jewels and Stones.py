1d23bfca8b55c195fca25e429ab32504ccd0bd2f
Huda Kukaswadia <huda.kukaswadia@hotmail.com>
2025-12-06 21:58:00 +0000

    Jewels and Stones

--- Arrays and Hashing/771. Jewels and Stones.py
+++ Arrays and Hashing/771. Jewels and Stones.py
@@ -1,3 +1,12 @@
+"""
+Why use a set? 
+- Converting jewels to a set optimizes search operations from O(n) linear search to O(1) constant-time lookups. 
+- Without a set, you would need nested loops O(n x m) time complexity. 
+
+"Time: O(J + S) where J is the length of jewels and S is the length of stones. 
+Space: (J) for storing the jewel set"
+"""
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         jewels = set(jewels)
